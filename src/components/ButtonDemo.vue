b6f8d5d2535e179b5eff0edd0789bd5c7bc1de98
zzz <17704904@qq.com>
2021-09-29 09:24:05 +0800

    dev错误

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,11 +5,26 @@
     <h1>示例1</h1>
     <div>
         <Button>你好</Button>
+        <Button theme="button">你好</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
+    </div>
+    <h1>示例2</h1>
+    <div>
+        <Button size="big">大大</Button>
+        <Button>正常</Button>
+        <Button size="small">小小</Button>
     </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue"
 export default {
     components: { Button },
+    setup(){
+        const onClick = ()=>{
+            console.log('hi');
+        }
+        return {onClick}
+    }
 };
 </script>
