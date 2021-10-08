9f5f231fea01a1d582cc65cf7387266fca501ce2
zzz <17704904@qq.com>
2021-10-08 16:16:22 +0800

    新增路由

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,39 +1,56 @@
 <template>
-    <button class="gulu-button" :class="classes">
-        <slot />
-    </button>
+  <button class="gulu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="gulu-loadingIndicator"></span>
+    <slot />
+  </button>
 </template>
+
 <script lang="ts">
-import { computed } from 'vue'
+import { computed } from "vue";
 export default {
-    props: {
-        theme: {
-            type : String,
-            default:"button",
-        },
-        size: {
-          type: String,
-          default:'normal'
-        },
+  props: {
+    theme: {
+      type: String,
+      default: "button",
     },
-    setup(props) {
-      const {theme,size} = props
-      const classes = computed(()=>{
-        return {
-          [`gulu-theme-${theme}`]: theme,
-          [`gulu-size-${size}`]: size,
-          };
-      });
-      return { classes };
-    }
-}
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
+      };
+    });
+    return { classes };
+  },
+};
 </script>
 <style lang="scss">
-    $h: 32px;
+$h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .gulu-button {
   box-sizing: border-box;
   height: $h;
@@ -48,6 +65,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -102,22 +120,22 @@ $radius: 4px;
       }
     }
     &.gulu-level-danger {
-      background: red;
-      border-color: red;
+      background: $red;
+      border-color: $red;
       color: white;
       &:hover,
       &:focus {
-        background: darken(red, 10%);
-        border-color: darken(red, 10%);
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
       }
     }
   }
   &.gulu-theme-link {
     &.gulu-level-danger {
-      color: red;
+      color: $red;
       &:hover,
       &:focus {
-        color: darken(red, 10%);
+        color: darken($red, 10%);
       }
     }
   }
@@ -126,46 +144,51 @@ $radius: 4px;
       color: $blue;
       &:hover,
       &:focus {
-        color: darken(blue, 10%);
+        color: darken($blue, 10%);
       }
     }
     &.gulu-level-danger {
-      color: red;
+      color: $red;
       &:hover,
       &:focus {
-        color: darken(red, 10%);
+        color: darken($red, 10%);
       }
     }
   }
   &.gulu-theme-button {
     &[disabled] {
       cursor: not-allowed;
-      color: grey;
+      color: $grey;
       &:hover {
-        border-color: grey;
+        border-color: $grey;
       }
     }
   }
-  &.gulu-theme-link, &.gulu-theme-text {
+  &.gulu-theme-link,
+  &.gulu-theme-text {
     &[disabled] {
       cursor: not-allowed;
-      color: grey;
+      color: $grey;
     }
   }
-  > .gulu-loadingIndicator{
+  > .gulu-loadingIndicator {
     width: 14px;
     height: 14px;
     display: inline-block;
     margin-right: 4px;
-    border-radius: 8px; 
-    border-color: blue blue blue transparent;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
     border-style: solid;
     border-width: 2px;
     animation: gulu-spin 1s infinite linear;
   }
 }
 @keyframes gulu-spin {
-  0%{transform: rotate(0deg)} 
-  100%{transform: rotate(360deg)} 
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
