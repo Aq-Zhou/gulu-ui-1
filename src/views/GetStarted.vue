7e234946cba108d04d4508420786f26c5c18e14b
zzz <17704904@qq.com>
2021-10-09 16:29:20 +0800

    简化 代码不过三

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,14 +1,1 @@
-<template>
-  <article class="markdown-body" v-html="md"></article>
-</template>
 
-<script>
-import md from "../markdown/get-started.md";
-export default {
-  data() {
-    return {
-      md,
-    };
-  },
-};
-</script>
