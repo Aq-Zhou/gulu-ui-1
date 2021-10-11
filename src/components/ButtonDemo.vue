5502564bb877b21cf08c90c15d509e674b4d2113
zzz <17704904@qq.com>
2021-10-11 17:37:23 +0800

    基本实现

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,30 +1,25 @@
 <template>
-    <div>
-        Button 示例
-    </div>
-    <h1>示例1</h1>
-    <div>
-        <Button>你好</Button>
-        <Button theme="button">你好</Button>
-        <Button theme="link">你好</Button>
-        <Button theme="text">你好</Button>
-    </div>
-    <h1>示例2</h1>
-    <div>
-        <Button size="big" :theme="text">大大</Button>
-        <Button :theme="text">正常</Button>
-        <Button size="small" :theme="text">小小</Button>
-    </div>
+  <div>
+    <h1>Button 示例</h1>
+    <Demo :component="Button1Demo" />
+    <Demo :component="Button2Demo" />
+    <Demo :component="Button3Demo" />
+    <Demo :component="Button4Demo" />
+    <Demo :component="Button5Demo" />
+  </div>
 </template>
+
 <script lang="ts">
-import Button from "../lib/Button.vue"
+import Demo from "./Demo.vue";
+import Button1Demo from "./Button1.demo.vue";
+import Button2Demo from "./Button2.demo.vue";
+import Button3Demo from "./Button3.demo.vue";
+import Button4Demo from "./Button4.demo.vue";
+import Button5Demo from "./Button5.demo.vue";
 export default {
-    components: { Button },
-    setup(){
-        const onClick = ()=>{
-            console.log('hi');
-        }
-        return {onClick}
-    }
+  components: { Demo },
+  setup() {
+    return { Button1Demo, Button2Demo, Button3Demo, Button4Demo, Button5Demo };
+  },
 };
 </script>
